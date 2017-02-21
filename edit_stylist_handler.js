eca925f85d9a98d16f3986b9df914d1602c51c4b
btc989 <bclar787@gmail.com>
2017-02-21 17:27:24 -0600

    for profile stylist page handles editing

---
+++ edit_stylist_handler.js
@@ -0,0 +1,3 @@
+document.getElementById("lgm-1").addEventListener("click", swapProfileInfo);
+document.getElementById("lgm-2").addEventListener("click", swapProfileInfo);
+document.getElementById("lgm-3").addEventListener("click", swapProfileInfo);
