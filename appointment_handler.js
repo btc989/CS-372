3e8795fe7e7125b46c9dbf2246bc691f272ccb3f
btc989 <bclar787@gmail.com>
2017-03-07 09:07:22 -0600

    added code to close all popups

--- appointment_handler.js
+++ appointment_handler.js
@@ -1,4 +1,6 @@
-
 document.getElementById("newAppointment").addEventListener("click", addAppointment);
 document.getElementById("selectedHairstyle").addEventListener("click", selectHairstyle);
 document.getElementById("selectedHairstylist").addEventListener("click", selectHairstylist);
+document.getElementsByClassName("close")[0].addEventListener("click",closeSelectHairstyle);
+document.getElementsByClassName("close")[1].addEventListener("click",closeSelectHairstylist);
+document.getElementsByClassName("close")[2].addEventListener("click",closeEvent);
